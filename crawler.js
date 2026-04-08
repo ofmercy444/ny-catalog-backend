d2124adf905f9a83bd658bf777cf9d9cde1257b5
Daniella <ofmercy444@gmail.com>
2026-04-08 14:25:00 -0400

    crawler: temporary hair-only accessory discovery

--- crawler.js
+++ crawler.js
@@ -179,15 +179,26 @@ const BODY_TERMS = {
   hair: ["hair", "hairstyle", "wig", "ponytail", "braid", "curly hair", "wavy hair"],
 };
 
+// Temporary focus mode: dedicate accessory discovery budget to HairAccessory (41).
 const ACCESSORY_TERMS_BY_TYPE = {
-  8: ["hat", "cap", "beanie", "hood", "crown"],
-  41: ["hair", "hairstyle", "wig", "ponytail", "braid", "curly", "wavy", "straight"],
-  42: ["face accessory", "mask", "glasses", "bangs", "fringe"],
-  43: ["necklace", "choker", "scarf", "collar"],
-  44: ["shoulder pet", "shoulder accessory", "pauldron"],
-  45: ["front accessory", "chain", "chest rig", "lanyard"],
-  46: ["back accessory", "wings", "cape", "backpack"],
-  47: ["waist accessory", "belt", "tail", "skirt accessory"],
+  41: [
+    "hair",
+    "hairstyle",
+    "hair accessory",
+    "wig",
+    "ponytail",
+    "pigtails",
+    "braid",
+    "bob",
+    "pixie",
+    "long hair",
+    "short hair",
+    "curly hair",
+    "wavy hair",
+    "straight hair",
+    "bangs",
+    "fringe",
+  ],
 };
 
 let consecutive429 = 0;
